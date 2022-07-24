63537ba7fe154f8d699572cfc8853adcd21a5cf4
jun-yuaka <yuaka0511@gmail.com>
2022-07-24 11:49:24 +0900

    init.vimのインストールスクリプト追加

--- setup.sh
+++ setup.sh
@@ -10,5 +10,11 @@ if [ -e ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.org
 fi
 
+# ~/.config/nvim/init.vimが存在したら~/.config/nvim/init.vim.orgにリネームして退避する
+if [ -e ~/.config/nvim/init.vim ]; then
+    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.org
+fi
+
 # シンボリックリンクを作成する
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
